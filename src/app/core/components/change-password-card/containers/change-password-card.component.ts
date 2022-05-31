d3e4f104f3e8f43e1b6d521a718fec0e9e7163cf
SBR118 <ninobr98@gmail.com>
2022-05-31 14:32:47 +0200

    UPDATE: ChangePassword

--- src/app/core/components/change-password-card/containers/change-password-card.component.ts
+++ src/app/core/components/change-password-card/containers/change-password-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { UserOutputDTO } from "../../../../shared/sdk";
 
 @Component({
   selector: 'change-password-card',
@@ -7,20 +8,38 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./change-password-card.component.scss']
 })
 export class ChangePasswordCardComponent implements OnInit {
-  pwdForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  pwdForm: FormGroup;
+  userData: UserOutputDTO;
+
+  constructor(private formBuilder: FormBuilder,) {
     this.pwdForm = this.formBuilder.group({
-      oldPassword: ['', [Validators.required]],
       newPassword: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_@./#&+-]{8,25}$')]],
       confirmPassword: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_@./#&+-]{8,25}$')]],
     });
+    this.userData = {};
   }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
+    /*const id = this.storageService.getItem('userId');
+
+    if(id) {
+      this.userService.apiUsersIdGet(id).subscribe((resp: UserOutputDTO) => {
+        this.userData = resp;
+      });
+    }
+
+    let datosActualizados: EditUserInputDTO = {};
     console.log(this.pwdForm.value);
+    (document.getElementById('editButton') as HTMLButtonElement).style.display = 'block';
+    (document.getElementById('saveButton') as HTMLButtonElement).style.display = 'none';
+    if(id)
+      this.userService.apiUsersIdPut(id,datosActualizados).subscribe(resp=>{
+        console.log(resp);
+      });*/
+    (document.getElementById("close") as HTMLButtonElement).click();
   }
 }
