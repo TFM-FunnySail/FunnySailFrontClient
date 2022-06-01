b909ef240590c1d1dbeb1e5bba7562bacededbb7
andresll706 <andresll706@gmail.com>
2022-06-02 00:55:52 +0200

    ADD: homecomponent

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
-import {HomeComponent} from './containers/home.component';
 import {HomeRoutingModule} from './home-routing.module';
 import {ReactiveFormsModule} from "@angular/forms";
 import {CoreModule} from "../../core/core.module";
 import {SharedModule} from "../../shared/shared.module";
+import {HomeComponent} from "./containers/home.component";
 
 @NgModule({
   declarations: [
